35bb7185da9b0abda4698ff35249eadbb826e8c4
Ranbir Paul <ranbir.paul@in.abb.com>
2018-11-26 15:50:11 +0530

    Latest code

--- UnitTestTenant/UnitTestRackService.cs
+++ UnitTestTenant/UnitTestRackService.cs
@@ -215,7 +215,7 @@ namespace UnitTestTenant
 		[TestMethod]
 		public void is_remove_rack_by_name_returns_value_as_true_with_existing_rack_name_deleted_with_name_as_Rack5()
 		{
-			// 1. Arrange to
+			// 1. Arrange to test method
 			// Create the instance of rack service
 			IRackService rackservice = new RackService(rackRepo);
 
